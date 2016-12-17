c8443fbcf02885f1a2a9f2334c9f0b38731cc147
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-12-17 22:18:41 +0800

    update

--- routes/ethereum.js
+++ routes/ethereum.js
@@ -6,14 +6,34 @@ var router = express.Router();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 var coinbase = web3.eth.coinbase;
 
-/* GET ethereum coinbase */
+/**
+ * GET ethereum coinbase
+ */
 router.get('/coinbase', function(req, res, next) {
-  res.send('coinbase: ' + coinbase);
+  res.json({'data' :{'coinbase': coinbase}});
 });
 
+/**
+ * GET ethereum account balance
+ */
 router.get('/balance', function(req, res, next) {
-  var balance = web3.fromWei(web3.eth.getBalance(coinbase), "ether").toString(10);
-  res.send('balance: ' + balance + ' ether');
+  var balance = web3.fromWei(web3.eth.getBalance(coinbase), "ether").toString(10) + ' ether';
+  res.json({'coinbase': coinbase, 'balance': balance});
+});
+
+// send transaction
+router.post('/transaction', function(req, res, next) {
+
+  res.json({'txHash': txHash});
+});
+
+// query contract
+router.get('/transaction', function(req, res, next) {
+  var txHash = req.query.txHash;
+  if (txHash == null || txHash == '' || txHash == undefined) {
+    res.json({'error': {'code': 100, 'message': 'txHash is null'}});
+  }
+  res.json({"txHash": txHash});
 });
 
 module.exports = router;
