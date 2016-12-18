f759c047949be8c9751226300ac1a532ad5cfc9e
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-12-18 23:21:06 +0800

    update

---
+++ worker/worker.js
@@ -0,0 +1,9 @@
+/**
+ * impliment Kafka
+ *
+ * 1. Client producer
+ * 2. Transaction consumer
+ * 3. Event consumer -> write to Database
+ * 4. Web3 event producer, include web3 error handling
+ * 
+ */
