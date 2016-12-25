9af0883e766da1f4f9415959a9aa89d1b16109ee
Yung chieh Tsai <x01ep23i@hotmail.com>
2016-12-26 01:44:06 +0800

    extract contract

---
+++ test/contractTest.js
@@ -0,0 +1,6 @@
+var ProofOfTransaction = require('../contract/proofoftransaction.js');
+
+var txHash = "12345";
+var data = "67890";
+//ProofOfTransaction.setData(txHash, data);
+ProofOfTransaction.setData(txHash, data);
