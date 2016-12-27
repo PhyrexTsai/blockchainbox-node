ebb54e74fb08625fc089cd5b21f252ff35506a57
Jason Wang <wangpiju0420@gmail.com>
2016-12-28 01:49:41 +0800

    finish producer.js

--- kafka/producer/producer.js
+++ kafka/producer/producer.js
@@ -16,10 +16,17 @@ function KafkaProducer() {
     
 }
 
-KafkaProducer.prototype.send = function(payloads) {
+KafkaProducer.prototype.send = function(key, data) {
+    message = {};
+    message[key] = data;
+
+    payloads = [
+        { topic: 'InsertQueue', messages: JSON.stringify(message)}
+    ];
+
     return producer.on('ready', function () {
         return producer.send(payloads, function (err, data) {
-            // FIXME error handling
+            // FIXME error handling 這邊如果有錯誤，可能要討論一下機制 是否重送？
             console.log(data);
             return data;
         });
